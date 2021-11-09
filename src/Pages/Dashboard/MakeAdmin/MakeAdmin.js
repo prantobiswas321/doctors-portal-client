553be76f3b11fb54d68060d108dfecda0337039a
prantobiswas321 <rahulbd321@gmail.com>
2021-11-09 17:55:22 +0600

    admin role update done

--- src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
 
 const MakeAdmin = () => {
+    const [email, setEmail] = useState('');
+
+    const handleOnBlur = e => {
+        console.log(e.target.value);
+        setEmail(e.target.value);
+    }
+    const handleAdminSubmit = e => {
+        const user = { email };
+        console.log('user', user);
+        fetch('http://localhost:5000/users/admin', {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            });
+        e.preventDefault();
+    }
     return (
         <div>
             <h2>Make an admin</h2>
+            <form onSubmit={handleAdminSubmit}>
+                <TextField
+                    label="Email"
+                    type="email"
+                    onBlur={handleOnBlur}
+                    variant="standard" />
+                <Button type="submit" variant="contained">Make Admin</Button>
+            </form>
         </div>
     );
 };
